fe3eb200634cfa417d0063808082da44509a6d1a
dnlkrgr <danielkrueger@protonmail.com>
2018-11-16 13:17:31 +0100

    Updated record.

--- Data/Database/Record.hs
+++ Data/Database/Record.hs
@@ -1,3 +1,22 @@
 module Data.Database.Record where
 
-data Record = Record
+import qualified Data.Map as M
+
+type FieldValues = M.Map String String
+
+newtype Fields = 
+    Fields {
+        unfields :: String
+    } deriving (Show, Eq)
+
+data Record = 
+    Record {
+          getFlds   :: Fields 
+        , getFldVls :: FieldValues
+    } deriving (Show, Eq)
+
+createRecord :: Fields -> FieldValues -> Record
+createRecord flds fldVls = Record flds fldVls
+
+selectFromRecord :: String -> Record -> Maybe String
+selectFromRecord qry rcd = M.lookup qry (getFldVls rcd)
