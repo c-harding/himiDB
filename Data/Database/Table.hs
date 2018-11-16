e1e54c19950153c62fa06b27c00126ae4a85f6dc
Dawid Furman <dfurmans@gmail.com>
2018-11-16 11:11:09 +0100

    Domain model -> changing type for Id in Table and add TableTypeClass

--- Data/Database/Table.hs
+++ Data/Database/Table.hs
@@ -1,14 +1,20 @@
 module Data.Database.Table where
 
-type Id = Int
+type Id = String
 type Field = [String] 
 type Record = (String, String)
 type Description = String
     
-data Table = Table {
-    id :: Id 
-    fields :: Field
-    records :: Record
-    description :: Description
-}
+data Table = Table 
+  { id :: Id 
+  , fields :: Field
+  , records :: Record
+  , description :: Description
+  }
 
+class TableTypeClass a where
+  insert :: a -> Table -> Table
+  select :: a -> Table -> Table
+  delete :: a -> Table -> Table
+  description :: a -> Table -> Table
+  
