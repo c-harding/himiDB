358329cb49ee60bf81d62b03ac6012aa7940ca27
Dawid Furman <dfurmans@gmail.com>
2018-11-16 10:57:43 +0100

    Table => Basic ADT

--- Table.hs
+++ Table.hs
@@ -1,3 +1,14 @@
 module Data.Database.Table where
 
-data Table = Table
+type Id = Int
+type Field = [String] 
+type Record = (String, String)
+type Description = String
+    
+data Table = Table {
+    id :: Id 
+    fields :: Field
+    records :: Record
+    description :: Description
+}
+
