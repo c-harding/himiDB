0fc22c8010d5e3e22ed99f6b7d516933fc4ed2e1
Dawid Furman <dfurmans@gmail.com>
2018-11-16 13:55:29 +0100

    Table support Int and String type for columns

--- Data/Database/Table.hs
+++ Data/Database/Table.hs
@@ -3,25 +3,23 @@
 module Data.Database.Table(Table(tableName), empty, Field, addRecord) where
 
 import Data.Database.Record(Record(..))
+data Type = IntRecord | StringRecord
 type Name = String
-type Field = String
-    
+type Field = (String, Type)
+type Description = String
+ 
 data Table = Table 
   { tableName :: Name 
   , fields :: [Field]
   , records :: [Record]
-  }
+  }  deriving (Show)
 
+-- Creates an empty Table
 empty :: String -> [Field] -> Table
 empty name fields = Table name fields []
 
 addRecord :: Record -> Table -> Maybe Table
 addRecord record table = Just table{records = record : records table}
-
--- class TableTypeClass a where
---   insert :: a -> Table -> Table
---   select :: a -> Table -> Table
---   delete :: a -> Table -> Table
 
 select :: [(String, String)] -> [String] -> Maybe [[String]]
 select constraints outputs = _
