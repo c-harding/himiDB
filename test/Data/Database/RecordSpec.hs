9a205bde7957192bce806012ea92acca916d8ab4
Charlie Harding <xsanda@me.com>
2018-11-17 15:04:41 +0100

    Fix duplicate test mess

--- test/Data/Database/RecordSpec.hs
+++ test/Data/Database/RecordSpec.hs
@@ -1,13 +1,16 @@
 module Data.Database.RecordSpec (spec) where
 
 import Test.Hspec
-import Data.Database.Record(Record)
 
 -- Hspec example 
 -- Usage with stack :: stack setup && stack test
  
-spec :: Spec
-spec = do
+main :: IO ()
+main = spec $ do
   describe "Data.Database.Record" $ do
     it "should return an empty Record" $ do
-      head [23 ..] `shouldBe` (23 :: Int)    
+      head [23 ..] `shouldBe` (23 :: Int)
+
+    --it "returns the first element of an *arbitrary* list" $
+      --property $ \x xs -> head (x:xs) == (x :: Int)
+    
