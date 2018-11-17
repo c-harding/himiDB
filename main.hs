7c926fe764e7b185d64284bbf49e5388dc9de055
dnlkrgr <danielkrueger@protonmail.com>
2018-11-17 10:50:23 +0100

    Fixed parsing for create

--- main.hs
+++ main.hs
@@ -1,24 +1,64 @@
 module Main where
 
 import Data.Database.Database
+import Data.Database.Record
+import Data.Database.Table
 import Control.Monad.State.Strict
 import System.Exit (exitSuccess)
+import Data.List (foldl')
+import Control.Monad
+
+-- parsing stuff
+import Text.Parsec
+import Text.Parsec.Token
 
 main :: IO ()
 main = return ()
 --main = evalStateT getInput []
 
---commands :: [(String, String, Int, StateT Database IO ())]
+--g :: [String] -> StateT Database IO ()
+--g wrds@(cmd:arguments) = foldl' f wrds commands
+--
+--f :: [String] -> [(String, String, Int, [String] -> StateT Database IO ())] -> StateT Database IO ()
+--f acc b = undefined
+--
+--commands :: [(String, String, Int, [String] -> StateT Database IO ())]
 --commands =
 --    [ ("create", "Create Table", 2, 
---        do
---            let (name:fields:[])= arguments
---            modify $ createTable name fields
---	)]
+--        \arguments -> 
+--            do
+--                let (name:fields:[])= arguments
+--                --modify $ createTable name fields
+--                modify $ createTable name [("asdf", IntRecord)]
+--)]
     --,("describe", "Describe Table",)
     --,("insert", "Insert Record", --    ),("delete", "Delete Record",)
     --,("select", "Select Record",)
     --,("exit", "Exit",)
+
+typeP :: Parsec String () Type
+typeP = do
+    s <- many1 alphaNum
+    case s of
+        "IntRecord"    -> return IntRecord
+        "StringRecord" -> return StringRecord
+        _              -> fail "String doesn't fit Type" 
+    
+fieldP :: Parsec String () Field
+fieldP = do
+    char '('
+    s <- many1 alphaNum
+    char ','
+    t <- typeP
+    char ')'
+    return (s,t)
+
+fieldsP :: Parsec String () [Field]
+fieldsP = do
+    char '['
+    fields <- fieldP `sepBy` (char ',')
+    char ']'
+    return fields
 
 getInput :: StateT Database IO ()
 getInput = do
@@ -29,7 +69,10 @@ getInput = do
         case cmd of
             "create"   -> do
                             let (name:fields:[])= arguments
-                            modify $ createTable name fields
+                                result = parse fieldsP "parseFields" fields
+                            case result of
+                                Left errMsg   -> pure ()
+                                Right fields' -> modify $ createTable name fields'
 --            "insert"   -> do
 --                            let (name:record:[]) = arguments
 --                            db <- get 
